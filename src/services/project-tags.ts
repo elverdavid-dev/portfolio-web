8c4f7f1369e2022e8174e5517db31bdf0d13a661
Elver David <elverdavid0839@gmail.com>
2024-11-14 19:49:43 -0500

    chore: :tada: begin a project

---
+++ src/services/project-tags.ts
@@ -0,0 +1,33 @@
+import astroIcon from '@/assets/icons/logos/astro.svg'
+import mongodbIcon from '@/assets/icons/logos/mongodb.svg'
+import nestIcon from '@/assets/icons/logos/nestjs.svg'
+import nextIcon from '@/assets/icons/logos/next.svg'
+import tailwindIcon from '@/assets/icons/logos/tailwind.svg'
+import typescriptIcon from '@/assets/icons/logos/typescript.svg'
+
+export const productTags = {
+  next: {
+    name: 'Next.js',
+    icon: nextIcon,
+  },
+  tailwind: {
+    name: 'Tailwind CSS',
+    icon: tailwindIcon,
+  },
+  typescript: {
+    name: 'TypeScript',
+    icon: typescriptIcon,
+  },
+  astro: {
+    name: 'Astro',
+    icon: astroIcon,
+  },
+  mongodb: {
+    name: 'MongoDB',
+    icon: mongodbIcon,
+  },
+  nest: {
+    name: 'Nest.js',
+    icon: nestIcon,
+  },
+}
---
+++ src/services/technologies.ts
@@ -0,0 +1,73 @@
+import type { ImageMetadata } from "astro"
+import NextjsLogo from '@/assets/icons/logos/next.svg'
+import AstrojsLogo from '@/assets/icons/logos/astro.svg'
+import ReactjsLogo from '@/assets/icons/logos/react.svg'
+import TailwindLogo from '@/assets/icons/logos/tailwind.svg'
+import TypescriptLogo from '@/assets/icons/logos/typescript.svg'
+import NestjsLogo from '@/assets/icons/logos/nestjs.svg'
+import NodejsLogo from '@/assets/icons/logos/node.svg'
+import JavascriptLogo from '@/assets/icons/logos/js.svg'
+import CssLogo from '@/assets/icons/logos/css.svg'
+import HtmlLogo from '@/assets/icons/logos/html.svg'
+import MongodbLogo from '@/assets/icons/logos/mongodb.svg'
+import GitLogo from '@/assets/icons/logos/git.svg'
+interface Technologies {
+  name: string
+  logo: ImageMetadata
+}
+
+export const technologies: Technologies[] = [
+  {
+    name: "Next js",
+    logo: NextjsLogo,
+  },
+  {
+    name: "Tailwind css",
+    logo: TailwindLogo
+  },
+  {
+    name: 'Typescript',
+    logo: TypescriptLogo,
+  },
+  {
+    name: 'Nest js',
+    logo: NestjsLogo,
+  },
+  {
+    name: "Astro js",
+    logo: AstrojsLogo
+  },
+  {
+    name: "React js",
+    logo: ReactjsLogo
+  },
+
+
+  {
+    name: 'Node js',
+    logo: NodejsLogo,
+  },
+  {
+    name: 'Git',
+    logo: GitLogo,
+  },
+  /*  
+  {
+     name: 'Javascript',
+     logo: JavascriptLogo,
+   },
+   {
+     name: 'Css',
+     logo: CssLogo,
+   },
+   {
+     name: 'Html',
+     logo: HtmlLogo,
+   },
+ 
+   {
+     name: 'Mongo DB',
+     logo: MongodbLogo,
+   },
+ */
+]
