c7d06e9cd1e1c00d843b22465cd1cffda8d7c1c8
Elver David <elverdavid0839@gmail.com>
2024-12-06 21:44:41 -0500

    refactor: refactor images

---
+++ src/services/services.ts
@@ -0,0 +1,31 @@
+import service1 from '@/assets/images/service-1.svg'
+import service2 from '@/assets/images/service-2.svg'
+import service3 from '@/assets/images/service-3.svg'
+import service4 from '@/assets/images/service-4.svg'
+
+export const services = [
+  {
+    title: "Desarrollo Frontend",
+    description:
+      "Desarrollo de páginas web modernas y optimizadas para una excelente experiencia de usuario, utilizando tecnologías como React, Next.js y Tailwind css.",
+    url_image: service1,
+  },
+  {
+    title: "Desarrollo Backend",
+    description:
+      "Creación de sistemas seguros, escalables y eficientes que gestionen datos y funcionalidades, implementados con Node.js y Nest.js.",
+    url_image: service2,
+  },
+  {
+    title: "Desarrollo Full Stack",
+    description:
+      "Desarrollo completo de aplicaciones web, integrando frontend y backend para ofrecer soluciones integrales.",
+    url_image: service3,
+  },
+  {
+    title: "Optimización SEO",
+    description:
+      "Mejora del posicionamiento de tu sitio web en los motores de búsqueda para atraer más visitas orgánicas y aumentar tu visibilidad en línea.",
+    url_image: service4,
+  },
+];
